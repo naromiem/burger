942c384dfa6968fd6ebbfffef69c5cf937a87c77
naromiem <71422309+naromiem@users.noreply.github.com>
2021-01-11 00:18:35 -0500

    updated connection to jawsdb

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,9 @@ CREATE DATABASE ps9oer4w1b50rgbt;
 USE ps9oer4w1b50rgbt;
 
 CREATE TABLE burgers (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(20) NULL,
     devoured BOOLEAN,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
